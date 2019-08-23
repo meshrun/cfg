726d4cfa2722a9507c4fa1097f4a0afe67b775f1
Chanwit Kaewkasi <chanwit@gmail.com>
2019-08-23 20:25:39 +0700

    add require command to obtain result from other scripts

--- src/main/groovy/io/meshrun/cfg/Runner.groovy
+++ src/main/groovy/io/meshrun/cfg/Runner.groovy
@@ -2,10 +2,16 @@ package io.meshrun.cfg
 
 class Runner {
 
+    static require = { cls ->
+        return cls.newInstance().run()
+    }
+
     static void main(String[] args) {
         String dir = new File(args[0]).getParentFile().getAbsolutePath()
         String name = new File(args[0]).getName()
-        new GroovyScriptEngine(dir).run(name, new Binding())
+        def binding = new Binding()
+        binding.setVariable("require", require)
+        new GroovyScriptEngine(dir).run(name, binding)
     }
 
 }
