f3389495f9430f93a18604d3844f340e3aa946b8
Chanwit Kaewkasi <chanwit@gmail.com>
2019-08-25 13:44:03 +0700

    add script to compile and pack .so into the executable

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 ./gradlew clean shadowJar
 cp build/libs/cfg-1.0-SNAPSHOT-all.jar bundle/app.jar
+
+(cd golang && ./build.sh)
+cp golang/libjson2hcl.so ./bundle/jre/lib/amd64/
 ./warp-packer --arch linux-x64 --input_dir bundle --exec run.sh --output mrcfg
