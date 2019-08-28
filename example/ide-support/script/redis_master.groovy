94cb20d4bb77498ad21f352eee3fb8c9a5b648c2
Chanwit Kaewkasi <chanwit@gmail.com>
2019-08-29 01:16:16 +0700

    add ide-support example

---
+++ example/ide-support/script/redis_master.groovy
@@ -0,0 +1,45 @@
+def redisLabels = {
+  app 'redis'
+  role 'master'
+  tier 'backend'
+}
+
+def deployment = new k8s.apps.v1.Deployment("redis-master", {
+  metadata {
+    labels {
+      app "redis"
+    }
+  }
+  spec {
+    selector {
+      matchLabels redisLabels
+    }
+    replicas 1
+    template {
+      metadata {
+        labels redisLabels
+      }
+    }
+    spec {
+      containers {
+        - {
+          name "master"
+          image "redis"
+          resources {
+            cpu "100m"
+            memory "100Mi"
+          }
+          ports {
+            - {
+              containerPort 6379
+              name "redis"
+            }
+          }
+        }
+      }
+    }
+  }
+})
+
+deployment.metadata["namespace"] = "default"
+print(deployment)
