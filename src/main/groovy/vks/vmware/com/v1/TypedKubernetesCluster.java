79fbd17fe5ba58a9464d46d6474d88a4a0ffd346
Chanwit Kaewkasi <chanwit@gmail.com>
2019-08-28 22:34:00 +0700

    implement an experiment vks CRD

---
+++ src/main/groovy/vks/vmware/com/v1/TypedKubernetesCluster.java
@@ -0,0 +1,28 @@
+package vks.vmware.com.v1;
+
+import groovy.lang.Closure;
+import groovy.lang.DelegatesTo;
+import k8s.typechecked.TypedMetadata;
+
+public class TypedKubernetesCluster {
+
+    public void metadata(@DelegatesTo(TypedMetadata.class) Closure metadata) {}
+    public void spec(@DelegatesTo(Spec.class) Closure spec) {}
+
+    public class Spec {
+
+        public class Topology {
+
+            public class Workers {
+                public void count(Integer value) {}
+                public void class_(String name) {}
+            }
+
+            public void workers(@DelegatesTo(Workers.class) Closure workers) {}
+        }
+
+        public void topology(@DelegatesTo(Topology.class) Closure topology) {}
+        public void distribution(String version) {}
+    }
+
+}
